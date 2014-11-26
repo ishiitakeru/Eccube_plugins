ad8298e01c83d282db621a8a84cb104a8e7a33a3
ISHII Takeru <takeru.pikul@gmail.com>
2014-11-26 11:26:51 +0900

    ECcube独自プラグインサンプル

---
+++ function.dump.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+/**
+ * 引数の配列をvar_dump()する。
+ * 
+ * @author ISHII Takeru
+ */
+function smarty_function_dump($params, &$smarty){
+	//引数は連想配列「$params」で受け取っている
+	$target_array = $params["target_array"];
+	var_dump($target_array);
+
+}//function
+
+/*
+//tpl側の書き方の想定
+<!--{dump target_array=$array}-->
+
+//このファイルの置き場所
+//\data\module\Smarty\libs\plugins
+*/
